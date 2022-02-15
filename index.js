74946e9a0fd5acb133eaa68f3e8962e7068e5549
Enejenej <horbyakb@gmail.com>
2022-02-15 12:13:33 +0200

    pochatok

---
+++ index.js
@@ -0,0 +1,529 @@
+/*let string = "string"
+
+
+console.log(typeof string);*/
+/*let i;
+let a = null;
+
+console.log(i, a);*/
+
+
+/*let i;
+let a = null;
+i = 5
+
+console.log(i, a);*/
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + number2;
+
+console.log(sum);*/
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + +number2;
+
+console.log(sum);*/
+
+
+/*alert("hello");*/
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + number2;
+
+console.log(typeof navber1, typeof navber2);*/
+
+
+/*let mynumber = 125;
+
+console.log(typeof mynumber);*/
+
+
+/*let mynumber = 125;
+
+console.log(typeof String (mynumber));
+
+console.log(String (mynumber));*/
+
+
+/*let mynumber = 125;
+
+let newnumber = String(mynumber);
+
+console.log(newnumber);*/
+
+
+/*let mynumber = "125";
+
+let newnumber = Number(mynumber);
+
+console.log(newnumber);*/
+
+
+/*let mynumber = "125";
+
+let newnumber = typeof Boolean(mynumber);
+
+console.log(newnumber);*/
+
+
+/*let mynumber = "125";
+
+alert(mynumber);*/
+
+
+/*let Number = prompt("Введіть число");
+console.log(Number);*/
+
+
+/*let Number1 = prompt("Введіть число");
+let Number2 = prompt("Введіть число", 5);
+let sum = Number1 + Number2;
+alert(sum);*/
+
+/*let Number1 = prompt("Введіть число");
+let Number2 = prompt("Введіть число", 11111);
+let sum = Number1 + Number2;
+alert(sum);*/
+
+/*let Number1 = +prompt("Введіть число");
+let Number2 = +prompt("Введіть число", 5);
+let sum = Number1 + Number2;
+alert(sum);*/
+
+/*додає*/
+/*let Number1 = Number(prompt("Введіть число"));
+let Number2 = Number(prompt("Введіть число", 5));
+let sum = Number1 + Number2;
+alert(sum);*/
+
+/*let guestion = confirm("Ваш вік 20?");
+
+console.log(guestion);*/
+
+/*let nun = 1, nun = "gue", res;
+console.log(res = nun * num);NaN*/
+
+/*let .nun = 1. men 1 = "gue" ,res;
+console.log(res = nun * nun1);*NaN//
+
+/*let Number = 1, secondNumber = "2", result;
+result = Number * secondNumber;
+console.log(result); //NaN*/
+
+/*undefined
+
+let nun = 1, nun1 = "gue", res;
+console.log(res = nun + nun1);
+
+/*let a = 2, b = 2;
+console.log(a >-b);true*/
+
+/*let a = 2, b = 2;
+console.log(a > b); false*/
+
+/*let a = "2", b = 2;
+console.log(a == b); //t*/
+
+/*undefined
+
+let a = "2", b = 2;
+console.log(a === b); //f
+undefined*/
+
+/*let a = "2", b = "2";
+console.log( a == b ); //t
+
+/*let a = "2", b = "3";
+console.log(a === b);//F*/
+
+/*let a = "gyerti", b = "gyerti";
+console.log(a === b);//t*/
+
+/*let a = "gyerti", b = "gyerti";
+console.log(a == b);//t*/
+
+/*!= - ==, !== - ===*/
+
+/*const a = 3, b ="3";
+
+/*console.log(a != b);//f 
+console.log(a !== b );//t*/
+
+/*let a = 1, b = 2;
+
+if ( a!==b ) {
+    console.log(true);
+} else {
+    console.log(false);
+}//t
+
+let a = 1, b = 2;
+
+if ( a===b ) {
+    console.log(true);
+} else {
+    console.log(false);
+}//f*/
+    /*let a = 1, b = 2;
+
+    if ( a ) {
+        console.log(true);
+    } else {
+        console.log(false)
+    }*/
+
+/*let a, b = 1;
+
+if (!a) {
+    console.log(true)
+} else {
+    console.log(false)
+}//ok*/
+
+/*let a = +prompt("age");
+let b = +prompt("newage");
+if (a == 20){
+    console.log("ur age is ; $(a)");
+} else if (a == 21){
+    console.log("ur age is ; $(a)");
+} else {
+    console.log("ur age is other ; $(a)");
+}*/
+
+/*let year = Number(prompt("input current year"));
+if(year === 2021) {
+    console.log("year this is current year!");
+} else if (!year){
+    console.log("please input something!");
+} else {
+    console.log("hope, you are wrong! Thisis not current year!");
+}*/
+
+/*let a = 4;
+
+switch(a) {
+    case 1:
+        console.log("result fo small");
+        break;
+        case 2:
+            console.log("you are close to correct an swer!");
+            break;
+    case 3:  
+                console.log("you are to close!");
+                break;
+    case 4:
+        console.log("you are right!");
+        break;
+
+    default:
+        console.log("you are wrong!");
+} */
+
+
+
+/*let a = +prompt("age");
+
+switch (a) {
+    case 1:
+        console.log("ur age is; $(a)")
+        
+        break;
+        case 2:
+            console.log("sadechur age is; $(a)")
+        break;
+        case 3:
+            console.log("urasdad age is; $(a)")
+            break;
+            case 4:
+                console.log("urrr age is; $(a)")
+        break;
+}*/
+
+/*let a = 2, b = 3;
+
+console.log(b < a);*/
+
+/*let a = 2, b = 3;
+
+console.log(b < a);*/
+
+/*let a = prompt("age");
+
+switch (a) {
+    case "hello":
+        console.log("ur age is; $(a)")
+        
+        break;
+        case 2:
+            console.log("sadechur age is; $(a)")
+        break;
+        case 3:
+            console.log("urasdad age is; $(a)")
+            break;
+            case 4:
+                console.log("urrr age is; $(a)")
+        break;
+}*/
+
+/*let a = +prompt("age");
+
+switch (a) {
+    case 1:
+        case 2:
+            console.log("sadechur age is; $(a)")
+        break;
+        case 3:
+            console.log("urasdad age is; $(a)")
+            break;
+            case 4:
+                console.log("urrr age is; $(a)")
+        break;
+}*/
+
+/*let a = +prompt("age");
+
+switch (a) {
+case 1:
+    console.log("ur age is; $(a)")
+    
+    break;
+    case 2:
+        console.log("sadechur age is; $(a)")
+    break;
+    case 3:
+        console.log("urasdad age is; $(a)")
+        break;
+        case 4:
+            console.log("urrr age is; $(a)")
+    break;
+    default:
+    console.log("you are wrong!");
+    break;
+}*/
+
+/*const year = Number(prompt("input current year; "));
+const result = (year === 2021) ? "corect answer!": "you are wrong!";
+console.log(result);*/
+
+/*let a = prompt("age");
+if ( a == 20 ) {
+    console.log("ur age is: $(a)");
+} else {
+    console.log("age is: $(a)");
+}*/
+
+/*let a = prompt("age");
+
+const result = ( a == 20 ) ? "ur age is: $(a)" : "age is: $(a)";
+
+console.log(result);*/ /*тернарний оператор*/
+
+/*let hour = 20, age = 21;
+
+if(hour > 20 || age >18){
+    alert("welcome to our party!");
+} else{
+    alert("sty butu can`t go to the party!")
+}*/
+  
+/*let hour = 20, age = 21;
+
+if(hour > 20 && age >18){
+    alert("welcome to our party!");
+} else {
+    alert("sty butu can't go to the party!")
+}*/
+
+/*if(a==2 || b==24 || c==25){
+console.log("hello");
+} else {
+    console.log("helloo");
+} ?*/
+
+/*if(a==2 && b==24 && c==25){
+console.log("hello");
+} else {
+    console.log("helloo");
+} 116xv.?*/
+
+/*let hour ;
+if (!hour) {
+    alert("ops, some thing wrong!");
+}*/
+
+//коментарі 
+
+/*const arr = ["javascript", "html", "css"];
+console.log(arr[0]);
+//alert(arr);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+arr[3] = "angular";
+console.log(arr[3]);
+console.log(arr);*/
+
+/*const obj = {
+    name: "sdad",
+    info: ["sdsd",[]]
+};
+console.log(obj);*/
+
+/*const arr = ["javascript", "html", "css"];
+console.log(arr);
+arr[2] = "angular";
+console.log(arr);*/
+
+
+/*const arr = ["javascript", "html", "css"];
+console.log(arr.length); //3*/
+
+/*const arr = ["javascript", "html", "css"];
+console.log(arr);
+const info = prompt("input String");
+
+console.log(arr.push(info));
+console.log(arr);*/
+
+/*const arr = ["javascript", "html", "css"];
+//const array = ["javascript", "html", "css"];
+console.log(arr);
+const info = prompt("input String");
+
+console.log(arr.push("sdsd"));
+console.log(arr);*/
+
+/*const arr = ["javascript", "html", "css"];
+const array = ["javascript", "html", "css"];
+
+console.log(arr);
+console.log(arr.push("array"));
+console.log(arr);*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+const array = ["JavaScript", "HTML", "CSS"];
+console.log(arr);
+console.log(arr.push("array"));
+console.log(arr);*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+for (i = 0; i < 10; i ++) {
+    console.log(i);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+for (i = 10; i > 0; i --) {
+    console.log(i);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+for (let i = 0; i < arr.length; i ++) {
+    console.log(i);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+
+for (let i = 0; i < arr.length; i ++) {
+    console.log(arr[i]);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let length = arr.length
+for (let i = 0; i < length; i ++) {
+    console.log(arr[i]);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+console.log(...arr);*/
+
+/*for (let i=0; i<5; i++) {
+    console.log(Math.floor(Math.random()*100));
+    
+}*/
+
+/*function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(3));
+  // expected output: 0, 1 or 2
+  
+  //console.log(getRandomInt(1));
+  // expected output: 0
+  
+  //console.log(Math.random());*/
+  // expected output: a number from 0 to <1
+
+/*console.log(Math.floor(5.95));
+// expected output: 5*/
+
+/*console.log(Math.floor(5.05));
+// expected output: 5
+
+console.log(Math.floor(5));
+// expected output: 5
+
+console.log(Math.floor(-5.05));
+// expected output: -6*/
+
+/*for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
+    alert(i);
+  }*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let i = 0;
+while (i < arr.length) {
+    console.log(arr[i]);
+    i++;
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let i = 0;
+while (i < 10) {
+    
+    console.log(i);
+    i++;
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let i = 0;
+while (true) {
+    console.log(arr[i]);
+    i++;
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let i = 0;
+while (i = 1) {??
+    console.log(arr[i]);
+    i++;
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i<10);*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+for(let i = 0; i < 10;i++) {
+    if (1==2) {
+       break; 
+    }
+    console.log(i);
+}*/
+
+/*const arr = ["JavaScript", "HTML", "CSS"];
+for(let i = 0; i < 10; i++) {
+    if ( 1 === 2 ) {
+       continue;
+    }
+    console.log(i);
+}??*/
